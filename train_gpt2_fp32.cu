d667ee22d5e39116b92389bf73a0c0a111ffe6c2
MasterSkepticista <kbshah1998@outlook.com>
2025-02-11 15:21:50 +0530

    Encoder forward in f32

--- train_gpt2_fp32.cu
+++ train_gpt2_fp32.cu
@@ -36,6 +36,49 @@ void cublasCheck(cublasStatus_t status, const char *file, int line) {
 #define cublasCheck(status) (cublasCheck(status, __FILE__, __LINE__))
 
 /**
+ * Ceildiv helper
+ */
+#define CEIL_DIV(x, y) (((x) + (y)-1) / (y))
+
+// Kernels
+
+__device__ inline float4 add_float4(const float4 &a, const float4 &b) {
+  return make_float4(a.x + b.x, a.y + b.y, a.z + b.z, a.w + b.w);
+}
+
+__global__ void encoder_forward_kernel3(float4 *out, const int *inp, const float4 *wte, const float4 *wpe, int B, int T,
+                                        int C) {
+  int C4 = C / 4;
+  int idx = blockIdx.x * blockDim.x + threadIdx.x;
+  int N = B * T * C4;
+  if (idx < N) {
+    int bt = idx / C4;
+    int b = bt / T;
+    int t = bt % T;
+    int c4 = idx % C4;
+    int ix = inp[b * T + t];
+    out[b * T * C4 + t * C4 + c4] = add_float4(wte[ix * C4 + c4], wpe[t * C4 + c4]);
+  }
+}
+// Launchers
+
+/**
+ * Encoder forward pass kernel launcher.
+ * @param out: ptr to store output tensor of shape (B, T, C).
+ * @param inp: ptr to input tokens tensor of shape (B, T).
+ * @param wte: ptr to token embedding weights.
+ * @param wpe: ptr to positional embedding weights.
+ */
+void encoder_forward(float *out, const int *inp, const float *wte, const float *wpe, int B, int T, int C) {
+  assert(C % 4 == 0);
+  const int block_size = 512;
+  const int N = B * T * C;
+  const int grid_size = CEIL_DIV(N / 4, block_size);
+  encoder_forward_kernel3<<<grid_size, block_size>>>((float4 *)out, inp, (float4 *)wte, (float4 *)wpe, B, T, C);
+  cudaCheck(cudaGetLastError());
+}
+
+/**
  * Program global cuBLAS handle.
  */
 static cublasComputeType_t cublas_compute_type;
@@ -360,6 +403,11 @@ void gpt2_forward(GPT2 *model, int *inputs, int *targets, int B, int T) {
     cudaCheck(cudaMemcpy(model->inputs, inputs, sizeof(int) * B * T, cudaMemcpyHostToDevice));
   }
 
+  // foward pass
+  ParameterTensors params = model->params;
+  ActivationTensors acts = model->acts;
+  float *residual;
+  encoder_forward(acts.encoded, model->inputs, params.wte, params.wpe, B, T, C);
   // encoder forward
   // loop - layers forward
   // final ln forward
